5087b4b081c9f61ef098d113577e1787402a3d78
RaevaaM <107184229+RaevaaM@users.noreply.github.com>
2022-11-25 09:39:54 +0200

    prop v VegetablesType

---
+++ GenericRepository/DataAccess/Model/VegetablesType.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Model
+{
+    public class VegetablesType
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public decimal Price { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
